14c292e319e4bc3cdd78b35536b9d69d0c1128e2
Mani Ratan <mani787060@gmail.com>
2026-04-08 08:23:45 +0530

    Added required resource

---
+++ pydanticoutputparser.py
@@ -0,0 +1,36 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# Define the model
+model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite-preview")
+
+
+class Person(BaseModel):
+
+    name: str = Field(description='Name of the person')
+    age: int = Field(gt=18, description='Age of the person')
+    city: str = Field(description='Name of the city the person belongs to')
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'sri lankan'})
+
+print(final_result)
+
+
+
+
+# So, using pydantic output parser, we can define a pydantic model that represents the structure of the data we want to extract from the model's response. The parser will then validate the model's response against this pydantic model and extract the information accordingly.
